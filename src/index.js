e8c639f6690eaa7f720de61851f92782b628272c
Saull Brandão <saull@outlook.com>
2022-01-06 08:53:42 -0300

    feat: middleware to check if a todo exists

--- src/index.js
+++ src/index.js
@@ -35,7 +35,30 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const isValidId = validate(id);
+
+  if (!isValidId) {
+    return response.status(400).json({ error: "Invalid todo ID" });
+  }
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  request.todo = todo;
+  request.user = user;
+  return next();
 }
 
 function findUserById(request, response, next) {
